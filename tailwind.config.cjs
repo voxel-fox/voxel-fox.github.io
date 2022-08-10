4add424c7f58e967ee15eed4db0b3c18fb15e603
Kara Todd <kara@voxel-fox.com>
2022-08-09 21:57:32 -0400

    use custom reset

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
     extend: {
       colors: {
