99a02662b61f50c36362bfbdfce2bb9d971dd3f2
Kara Todd <kara@voxel-fox.com>
2022-08-09 20:54:43 -0400

    remove old website

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import { ThemeProvider } from "next-themes";
-
-const App = ({ Component, pageProps }) => (
-  <ThemeProvider forcedTheme={Component.theme || undefined}>
-    <Component {...pageProps} />
-  </ThemeProvider>
-);
-
-export default App;
