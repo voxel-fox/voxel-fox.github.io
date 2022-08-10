9c929454fbfb9050337207785475aad30787fe32
Kara Todd <kara@voxel-fox.com>
2022-08-09 21:41:14 -0400

    add favicons and base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
+const dev = process.env.NODE_ENV === "development";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -18,6 +20,10 @@ const config = {
       fallback: null,
       precompress: false,
     }),
+
+    paths: {
+      base: dev ? "" : "/website",
+    },
 
     prerender: {
       default: true,
