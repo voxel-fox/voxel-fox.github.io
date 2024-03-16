4b1f06ac23b806814e1d7ec02634e44131716a5e
Kara Todd <kara@voxel-fox.com>
2024-03-15 21:02:54 -0400

    ditch yarn (again) and update github actions

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,12 +14,13 @@
 />
 <DarkModeToggle />
 
-<main class="flex flex-col justify-center items-center gap-12 pt-[5vh]">
+<main class="flex flex-col justify-center items-center gap-12 pt-[5vh] min-h-screen">
   <h1 class="flex flex-col justify-center items-center">
     <VoxelFoxMark class="w-[15vw] mb-10" aria-hidden="true" />
     <VoxelFoxLogo class="w-[30vw]" />
   </h1>
   <p class="text-2xl text-center">Making things on the internet.</p>
+
   <p class="flex flex-wrap gap-8 items-center justify-center">
     <GitHubLink href="https://github.com/voxel-fox" title="GitHub: Voxel Fox">
       <VoxelFoxMark
@@ -35,4 +36,6 @@
       <Avatar class="relative z-2 translate-x-1" width="80" height="80" />
     </GitHubLink>
   </p>
+
+  <p class="text-base text-center mt-auto">Get in touch! <a class="inline-block py-1 border-b border-[#E46B2A] font-bold text-[#E46B2A] hover:border-transparent" href="mailto:hello@voxel-fox.com">hello@voxel-fox.com</a></p>
 </main>
